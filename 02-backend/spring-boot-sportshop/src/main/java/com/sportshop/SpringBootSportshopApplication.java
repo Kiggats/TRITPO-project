1cb3c9b8eb65ab7f13cc19af57353865d4aa6b4f
Nikita <nikita.covalevi4@gmail.com>
2021-10-07 21:56:23 +0300

    Initial commit. Created databases, product and product-category entity. git st

---
+++ 02-backend/spring-boot-sportshop/src/main/java/com/sportshop/SpringBootSportshopApplication.java
@@ -0,0 +1,13 @@
+package com.sportshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSportshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSportshopApplication.class, args);
+	}
+
+}
---
+++ 02-backend/spring-boot-sportshop/src/test/java/com/sportshop/SpringBootSportshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSportshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
