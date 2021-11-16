402f53edd2a99e16aebe09a0cdfb5d69c6e3c617
Nikita Kavalevich <nikita.covalevich@gmail.com>
2021-11-16 17:31:16 +0300

    dynamic sidebar

--- 02-backend/spring-boot-sportshop/src/main/java/com/sportshop/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-sportshop/src/main/java/com/sportshop/config/MyDataRestConfig.java
@@ -2,14 +2,29 @@ package com.sportshop.config;
 
 import com.sportshop.entity.Product;
 import com.sportshop.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig  implements RepositoryRestConfigurer {
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
@@ -28,5 +43,20 @@ public class MyDataRestConfig  implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>>  entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClass = new ArrayList<>();
+
+        for(EntityType entityType: entities) {
+            entityClass.add(entityType.getJavaType());
+        }
+
+        Class[] domenTypes = entityClass.toArray(new Class[0]);
+        config.exposeIdsFor(domenTypes);
     }
 }
