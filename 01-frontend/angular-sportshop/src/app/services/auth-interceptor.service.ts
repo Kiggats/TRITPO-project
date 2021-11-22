9f326736f4b11c01d04907f5bff59c78ddc561f9
Nikita Kavalevich <nikita.covalevich@gmail.com>
2021-11-22 16:30:04 +0300

    order history secure front

---
+++ 01-frontend/angular-sportshop/src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {from, Observable} from "rxjs";
+import {OktaAuth} from "@okta/okta-auth-js";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor{
+
+  constructor(private oktaAuth: OktaAuth) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request ,next))
+  }
+
+  private async handleAccess(request:HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    const securityEndpoints = ['http://localhost:8080/api/orders'];
+
+    if(securityEndpoints.some(url => request.urlWithParams.includes(url))) {
+      const accessToken = await this.oktaAuth.getAccessToken();
+
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    }
+    return next.handle(request).toPromise();
+  }
+}
